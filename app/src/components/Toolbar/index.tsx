108e2c6231a4fd379c05e65258f180ddf3d94529
Marc Berchtold <me@echolot.io>
2020-01-02 22:34:59 +0100

    Toolbar restyling

--- app/src/components/Toolbar/index.tsx
+++ app/src/components/Toolbar/index.tsx
@@ -8,13 +8,6 @@ class Toolbar extends React.PureComponent<{}, {}> {
   public render(): ReactNode {
     return (
       <div className={styles.Container}>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className={styles.FilterDef}>
-          <filter id="white-background-filter" x="0" y="0" width="100%" height="100%">
-            <feFlood floodColor="#ffffff" floodOpacity="0.5" result="flood" />
-            <feComposite in="SourceGraphic" in2="flood" result="composite" />
-          </filter>
-        </svg>
-        <div className={`${styles.ToolbarContainer} ${styles.Brightener}`} />
         <div className={styles.ToolbarContainer}>
           <ul className={styles.Toolbar}>
             <li>
